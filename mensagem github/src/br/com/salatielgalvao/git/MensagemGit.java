4ae3267d03a25908e9de9f9b1a9381040cb2cce7
Salatiel Galvão Zambelino <84423199+salgal42@users.noreply.github.com>
2023-05-14 19:56:08 -0300

    Adicionei arquivos via upload

---
+++ mensagem github/src/br/com/salatielgalvao/git/MensagemGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.salatielgalvao.git;
+
+/**
+ * @author Salatiel Galvão Zambelino
+ * Exercício 01 Mensagem
+ */
+public class MensagemGit {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Para aprender lógica é necessário resolver todos os exercícios. ");
+		
+
+	}
+
+}
